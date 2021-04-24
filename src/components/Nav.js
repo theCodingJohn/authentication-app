26463df0925cb8ceab6f740373f8bcfe5eaa1cf3
John Carlo Cunanan <codingjohn@outlook.com>
2021-04-24 20:20:30 +0800

    Add navlinks component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 import logo from "../assets/images/devchallenges.svg";
 import dropDown from "../assets/images/arrow-drop-down-black.svg";
+import { AccountIcon, GroupIcon, LogoutIcon } from "../svg";
 
 const Nav = () => {
+  const [display, setDisplay] = useState("none");
   const user = useSelector(({ userData }) => userData);
+
+  const toggle = () => {
+    display === "none" ? setDisplay("block") : setDisplay("none");
+  };
+
+  const logout = () => {
+    window.localStorage.removeItem("loggedUser");
+    window.location.href = "/login";
+  };
 
   return (
     <nav className="nav">
@@ -13,7 +24,7 @@ const Nav = () => {
         <div className="col col1">
           <img className="logo" src={logo} alt="devchallenges logo" />
         </div>
-        <div className="col col2">
+        <div className="col col2" onClick={toggle}>
           <img
             className="profile-pic"
             src={user.avatar}
@@ -25,6 +36,31 @@ const Nav = () => {
             src={dropDown}
             alt="black drop-down icon"
           />
+          <div className="nav-links-wrapper" style={{ display: `${display}` }}>
+            <div className="row">
+              <div className="col">
+                <AccountIcon />
+
+                <p>My Profile</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <GroupIcon />
+
+                <p>Group Chat</p>
+              </div>
+            </div>
+            <div className="row row3">
+              <div className="divider"></div>
+            </div>
+            <div onClick={logout} className="row row4">
+              <div className="col">
+                <LogoutIcon />
+                <p>Logout</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
--- src/svg/index.js
+++ src/svg/index.js
@@ -2,5 +2,16 @@ import GoogleIcon from "./GoogleIcon";
 import FacebookIcon from "./FacebookIcon";
 import TwitterIcon from "./TwitterIcon";
 import GithubIcon from "./GithubIcon";
+import GroupIcon from "./GroupIcon";
+import AccountIcon from "./AccountIcon";
+import LogoutIcon from "./LogoutIcon";
 
-export { GoogleIcon, FacebookIcon, TwitterIcon, GithubIcon };
+export {
+  GoogleIcon,
+  FacebookIcon,
+  TwitterIcon,
+  GithubIcon,
+  GroupIcon,
+  AccountIcon,
+  LogoutIcon,
+};
